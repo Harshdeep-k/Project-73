2d02fc8261f4b41d31f0d7d127993f9410180c12
Harshdeep-k <68320148+Harshdeep-k@users.noreply.github.com>
2021-05-20 13:40:40 +0530

    Add files via upload

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MyHeader from "../components/MyHeader"
+
+export default class ReadStoryScreen extends React.Component {
+  render() {
+  return (
+  
+   
+  <View>
+  <MyHeader/>
+      <Text>  Read a story here</Text>
+      </View>
+  
+    
+  )
+  }
+  
+}
